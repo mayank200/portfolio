a938e15fb340ba845d4406327ece4cf70d5fe516
mayank200 <mayanksha200@gmail.com>
2026-03-19 14:12:12 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// import("./src/env.js");
 import WithPWA from "next-pwa";
 
 const withPWA = WithPWA({
